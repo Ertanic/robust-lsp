b107d10472ab47ee8caecc7499943db9cf309054
Ertanic <black.ikra.14@gmail.com>
2024-11-16 23:34:43 +0300

    Implemented jumping to localization key definition from yaml files

--- src/goto/yml.rs
+++ src/goto/yml.rs
@@ -1,9 +1,12 @@
 use super::{GotoDefinition, GotoDefinitionResult};
 use crate::{
-    backend::{CsharpClasses, YamlPrototypes},
+    backend::{CsharpClasses, FluentLocales, YamlPrototypes},
     parse::{
         common::{DefinitionIndex, Index},
-        structs::csharp::{Component, Prototype, ReflectionManager},
+        structs::{
+            csharp::{Component, Prototype, ReflectionManager},
+            fluent::FluentKey,
+        },
     },
     utils::block,
 };
@@ -11,12 +14,13 @@ use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use ropey::Rope;
 use stringcase::camel_case;
 use tokio::task::block_in_place;
-use tower_lsp::lsp_types::{self, GotoDefinitionResponse, Location, Position, Url};
+use tower_lsp::lsp_types::{self, GotoDefinitionResponse, Location, LocationLink, Position, Url};
 use tree_sitter::{Node, Parser, Point, Tree};
 
 pub struct YamlGotoDefinition {
     classes: CsharpClasses,
     prototypes: YamlPrototypes,
+    locales: FluentLocales,
     position: Position,
     src: String,
     tree: Tree,
@@ -34,8 +38,12 @@ impl GotoDefinition for YamlGotoDefinition {
 
         let nest = self.get_nesting(&found_node);
         match nest {
-            2 => self.try_goto_prototype_definition(found_node),
-            4 => self.try_goto_component_definition(found_node),
+            2 => self
+                .try_goto_locid_definition(found_node, nest)
+                .or_else(|| self.try_goto_prototype_definition(found_node)),
+            4 => self
+                .try_goto_locid_definition(found_node, nest)
+                .or_else(|| self.try_goto_component_definition(found_node)),
             _ => None,
         }
     }
@@ -45,6 +53,7 @@ impl YamlGotoDefinition {
     pub fn new(
         classes: CsharpClasses,
         prototypes: YamlPrototypes,
+        locales: FluentLocales,
         position: Position,
         rope: &Rope,
     ) -> Self {
@@ -57,16 +66,99 @@ impl YamlGotoDefinition {
         Self {
             classes,
             prototypes,
+            locales,
             position,
             src,
             tree,
         }
     }
 
-    fn try_goto_component_definition(
-        &self,
-        found_node: Node<'_>,
-    ) -> Option<GotoDefinitionResponse> {
+    #[tracing::instrument(skip(self), ret)]
+    fn try_goto_locid_definition(&self, found_node: Node<'_>, nest: usize) -> GotoDefinitionResult {
+        let block_mapping_pair = {
+            let mut node = found_node;
+            while let Some(n) = node.parent() {
+                node = n;
+                if n.kind() == "block_mapping_pair" {
+                    break;
+                }
+            }
+            node
+        };
+
+        if block_mapping_pair.kind() != "block_mapping_pair" {
+            tracing::trace!("not a block mapping pair");
+            return None;
+        }
+
+        let key_node = block_mapping_pair.child_by_field_name("key")?;
+        let key_name = key_node.utf8_text(self.src.as_bytes()).ok()?;
+
+        if key_name == found_node.utf8_text(self.src.as_bytes()).ok()? {
+            return None;
+        }
+
+        let value_node = block_mapping_pair.child_by_field_name("value")?;
+        let value = value_node.utf8_text(self.src.as_bytes()).ok()?;
+
+        match nest {
+            2 => {
+                let proto_name = self
+                    .get_field(&block_mapping_pair.parent()?, "type")?
+                    .child_by_field_name("value")?
+                    .utf8_text(self.src.as_bytes())
+                    .ok()?;
+
+                let reflection = ReflectionManager::new(self.classes.clone());
+                let proto = block(|| reflection.get_prototype_by_name(proto_name))?;
+
+                let field = block(|| reflection.get_fields(&proto))
+                    .into_iter()
+                    .find(|f| f.get_data_field_name() == key_name)?;
+
+                if field.type_name != "LocId" {
+                    tracing::trace!("not a locid field");
+                    return None;
+                }
+
+                let lock = block_in_place(|| self.locales.blocking_read());
+                let locale = lock.get(&FluentKey::dummy(value))?;
+
+                let location = get_location_link(locale.index(), value_node)?;
+
+                Some(GotoDefinitionResponse::Link(vec![location]))
+            }
+            4 => {
+                let comp_name = self
+                    .get_field(&block_mapping_pair.parent()?, "type")?
+                    .child_by_field_name("value")?
+                    .utf8_text(self.src.as_bytes())
+                    .ok()?;
+
+                let reflection = ReflectionManager::new(self.classes.clone());
+                let comp = block(|| reflection.get_component_by_name(comp_name))?;
+
+                let field = block(|| reflection.get_fields(&comp))
+                    .into_iter()
+                    .find(|f| f.get_data_field_name() == key_name)?;
+
+                if field.type_name != "LocId" {
+                    tracing::trace!("not a locid field");
+                    return None;
+                }
+
+                let lock = block_in_place(|| self.locales.blocking_read());
+                let locale = lock.get(&FluentKey::dummy(value))?;
+
+                let location = get_location_link(locale.index(), value_node)?;
+
+                Some(GotoDefinitionResponse::Link(vec![location]))
+            }
+            _ => None,
+        }
+    }
+
+    fn try_goto_component_definition(&self, found_node: Node<'_>) -> GotoDefinitionResult {
         let seeking = found_node.utf8_text(self.src.as_bytes()).ok()?;
 
         let mapping_pair_node = {
@@ -217,7 +309,7 @@ impl YamlGotoDefinition {
         }
     }
 
-    fn index_to_definition(&self, index: &DefinitionIndex) -> Option<GotoDefinitionResponse> {
+    fn index_to_definition(&self, index: &DefinitionIndex) -> GotoDefinitionResult {
         let url = Url::from_file_path(index.0.clone()).ok()?;
         let (start_position, end_position) = {
             let range = index.1?;
@@ -269,4 +361,37 @@ impl YamlGotoDefinition {
         }
         None
     }
+}
+
+fn get_location_link(index: &DefinitionIndex, node: Node) -> Option<LocationLink> {
+    let DefinitionIndex(path, Some(locale_range)) = index else {
+        return None;
+    };
+    let url_range = lsp_types::Range {
+        start: Position {
+            line: node.start_position().row as u32,
+            character: node.start_position().column as u32,
+        },
+        end: Position {
+            line: node.end_position().row as u32,
+            character: node.end_position().column as u32,
+        },
+    };
+    let selection_range = lsp_types::Range {
+        start: Position {
+            line: locale_range.start_point.row as u32,
+            character: locale_range.start_point.column as u32,
+        },
+        end: Position {
+            line: locale_range.end_point.row as u32,
+            character: locale_range.end_point.column as u32,
+        },
+    };
+
+    Some(LocationLink {
+        origin_selection_range: Some(url_range),
+        target_uri: Url::from_file_path(path).ok()?,
+        target_selection_range: selection_range,
+        target_range: selection_range,
+    })
 }
